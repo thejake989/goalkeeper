86cb66a2c24f13ba9c022fe5d92c5973fa1890e4
jake gibson <thejakethree989@gmail.com>
2024-06-26 15:45:10 -0500

    Added root route and fixed environment variables

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path'); // Added for serving static files
 const User = require('./models/user');
 const Goal = require('./models/Goal');
 
@@ -14,6 +15,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 
 // Environment variables
 const mongoURI = process.env.MONGO_URI;
